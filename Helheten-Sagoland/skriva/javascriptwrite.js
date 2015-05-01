08f62ae497e2c8ec5ae6302524a50e6b19cadfe1
Hejhej <mscs_@hotmail.com>
2015-05-01 18:12:42 +0200

    LÄgger upp det här nu
    Ändrat Menyn, lagt till så att sidan hoppar direkt till boken.

---
+++ Helheten-Sagoland/skriva/javascriptwrite.js
@@ -0,0 +1,147 @@
+/*-- Funktionen som döljer bilder! */
+
+function toggle_div_fun(id) {
+
+	var divelement = document.getElementById(id)
+	if(divelement.style.display == 'block')
+		divelement.style.display = 'none';
+	else
+		divelement.style.display = 'block';
+}
+/*-- Funktionen som gör det möjligt att dra och släppa bilder */
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+
+
+function saveStoryTextAsFile()
+{
+	var textToWrite = document.getElementById("story_text").value;
+	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+	var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+
+	var downloadLink = document.createElement("a");
+	downloadLink.download = fileNameToSaveAs;
+	downloadLink.innerHTML = "Download File";
+	if (window.webkitURL != null)
+	{
+		/* behöver för att chrome ska tillåta den att fungera*/
+
+		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+	}
+	else
+	{
+		// Vid Firefox måste länken för att lägga till DOM innan man kan klicka */
+
+		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+		downloadLink.onclick = destroyClickedElement;
+		downloadLink.style.display = "none";
+		document.body.appendChild(downloadLink);
+	}
+
+	downloadLink.click();
+}
+
+function destroyClickedElement(event)
+{
+	document.body.removeChild(event.target);
+}
+
+function loadFileAsText()
+{
+	var fileToLoad = document.getElementById("fileToLoad").files[0];
+
+	var fileReader = new FileReader();
+	fileReader.onload = function(fileLoadedEvent) 
+	{
+		var textFromFileLoaded = fileLoadedEvent.target.result;
+		document.getElementById("inputTextToSave").value = textFromFileLoaded;
+	}
+
+	fileReader.readAsText(fileToLoad, "UTF-8");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*-- Alla färger du kan välja på för att ändra texten! */
+
+
+function set_color_red(id) {
+	document.getElementById("story_text").style.color = "#FF0D00"
+}
+
+function set_color_green(id) {
+	document.getElementById("story_text").style.color = "#14D100"
+}
+
+function set_color_pink(id) {
+	document.getElementById("story_text").style.color = "#F76F87"
+}
+
+function set_color_blue(id) {
+	document.getElementById("story_text").style.color = "#0F4FA8"
+}
+
+function set_color_black(id) {
+	document.getElementById("story_text").style.color = "#000000"
+}
+
+function set_color_orange(id) {
+	document.getElementById("story_text").style.color = "#FF7A00"
+}
+
+/*-- Ändra typsnitt funktion --*/
+
+function set_font_impact(id) {
+	document.getElementById("story_text").style.fontFamily= "impact"
+}
+
+function set_font_georgia(id) {
+	document.getElementById("story_text").style.fontFamily= "Georgia"
+	
+}
+
+function set_font_algerian(id) {
+	document.getElementById("story_text").style.fontFamily= "algerian"
+}
+
+function set_font_ComicSansMS(id) {
+	document.getElementById("story_text").style.fontFamily= "Comic Sans MS"
+}
+
+
+window.addEventListener("load")
