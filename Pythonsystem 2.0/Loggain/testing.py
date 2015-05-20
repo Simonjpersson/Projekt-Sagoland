f94759d54f9b59a908b1276cec23ee478eefb797
MarielleML <marielle_804@hotmail.com>
2015-05-20 14:59:36 +0200

    o
    o

--- Pythonsystem 2.0/Loggain/testing.py
+++ Pythonsystem 2.0/Loggain/testing.py
@@ -2,28 +2,24 @@ from bottle import route, run, template, request, static_file
 #import mysql.connector
 # Author: Marielle 
 """from validate_email import validate_email"""
+"""Den klassen testar att ansluta sig till index-sidan """
 
 """Puts in CSS-files and pics"""
-@route('/static/<filename>')
-def serve_static(filename):
-    return static_file(filename, root ="static")
+class testing():
+    @route('/static/<filename>')
+    def serve_static(filename):
+        return static_file(filename, root ="static")
 
 
 """Handels database connection """
-def handelsUsers():
-    con=mysql.connector.connect(username=username, mejl=mejl, password1=password1, host="localhost", database="sagoland")
-    cursor=con.cursor()
+    def handelsUsers():
+        con=mysql.connector.connect(username=username, mejl=mejl, password1=password1, host="localhost", database="sagoland")
+        cursor=con.cursor()
 
 """Homepage """
-@route("/")
-def homepage():
+    @route("/")
+    def homepage():
 
-    return template("index")
+        return template("index")
 
-
-@route("/contact1/")
-def contactus():
-
-    return template("contact1")
-
-run(reloader=True, debug=True, host="localhost", port =8080)
+    run(reloader=True, debug=True, host="localhost", port =8080)
